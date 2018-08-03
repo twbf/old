343fa4506f6396d3d5ee5d16a01e5df7cfc6d0ee
Thomas Bueler <twbueler@gmail.com>
2018-08-03 13:55:12 -0800

    mostly fixed stuff

--- jill/ordering-boxes.js
+++ jill/ordering-boxes.js
@@ -15,36 +15,24 @@ window.onload = function (evt) {
     Array.prototype.min = function() {
       return Math.min.apply(null, this);
     };
-    function style(sidebarChange){
+    function style(){
         var clums = [],
-            colls = Math.floor(window.innerWidth/430),
+            colls = Math.floor(window.innerWidth/350),
             width = window.innerWidth;
-            if (width<=1100 || !sidebarChange){
+            if (width<=1100){
                 colls = Math.floor(width/305);
             }
         for(i = 0; i < colls; i++) {
             clums[i] = 10;
         }
 
-        //get the sidebar to close when the window is less that 1100 px wide
-        if (width<=1100 && sidebarChange){
-            document.getElementById("checkBox").checked = false;
-        }else if(sidebarChange) {
-            document.getElementById("checkBox").checked = true;
-        }
+
 
         var k, obj, collom,
-            wW = width - 310;
-            if (width<=1100 || !sidebarChange){
-                wW = width-40;
-                document.getElementById("m").style.left = 20+'px'
-                document.getElementById("m").style.width = wW+'px'
-                document.getElementById("n").style.left = 10+'px'
-            } else{
-                document.getElementById("m").style.left = 280+'px'
-                document.getElementById("m").style.width = wW+'px'
-                document.getElementById("n").style.left = 270+'px'
-            }
+            wW = width-40;
+            document.getElementById("m").style.left = 10+'px'
+            document.getElementById("m").style.width = wW+'px'
+            document.getElementById("n").style.left = 0+'px'
         for(i = 1; i <post.length+1; i++) {
             id = 'p' + i;
             k = clums.indexOf(clums.min());
@@ -57,15 +45,7 @@ window.onload = function (evt) {
         }
     }
     $(window).resize(function(){
-        style(true);
+        style();
     });
-    $("#checkBox").change(function() {
-        if(!(this.checked)) {
-            style(false);
-        }
-        if(this.checked && window.innerWidth>1100) {
-            style(true);
-        }
-    });
-    style(true);
+    style();
 };
