7dc83ced972ed5aae0e805dd6334165a8204c01c
Thomas Bueler <twbueler@gmail.com>
2018-08-12 15:39:58 -0800

    modern layout

--- php-book/0391143 Ch11/db.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('MYSQL_HOST','localhost');
-define('MYSQL_USER','bp6am');
-define('MYSQL_PASSWORD','bp6ampass');
-define('MYSQL_DB','comicbook_fansite');
-?>
--- php-book/0391143 Ch11/firstmail.php
+++
@@ -1,4 +0,0 @@
-<?php
-mail('myaddress@example.com', 'Hello World',
-    'Hi, world. Prepare for our arrival. We are starving!');
-?>
